677314e88d9502fac68ea7738dc5e3a37f970aea
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-05-18 16:46:07 -0400

    add test seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(nickname: "Alex", email: "alex@gmail.com", password: "123", password_confirmation: "123", first_name: "Alex", last_name: "Rogachev")
+User.create(nickname: "Ksu", email: "ksu@gmail.com", password: "123", password_confirmation: "123", first_name: "Kseniia", last_name: "Gromova")
+User.create(nickname: "Luda", email: "luda@gmail.com", password: "123", password_confirmation: "123", first_name: "Ludmila", last_name: "Rubtsova")
+
+Chatroom.create(owner: User.first, title: "Hey lets go to a cinema tonight")
+
+UserChatroom.create(chatroom: Chatroom.first, user: User.all[1])
+UserChatroom.create(chatroom: Chatroom.first, user: User.all[2])
+
+Message.create(chatroom: Chatroom.first, user: User.all[0], content: "What do you think?")
+Message.create(chatroom: Chatroom.first, user: User.all[1], content: "Sure!")
