70321d493e373b6965886aaa37eea033eb630347
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-05-21 18:58:58 -0400

    finished basic chat funcionality using action cable(remove msg routes and add authorizations)

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,23 @@
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+    chatroom = Chatroom.find_by(id: params[:chatroom_id])
+    return if !chatroom ||
+              (chatroom.owner != current_user && !chatroom.guests.include?(current_user))
+
+    stream_from "chatroom_#{params[:chatroom_id]}:messages"
+  end
+
+  def receive(payload)
+    chatroom = Chatroom.find_by(id: params[:chatroom_id])
+    return if !chatroom ||
+              (chatroom.owner != current_user && !chatroom.guests.include?(current_user))
+
+    message = Message.new(user: current_user, chatroom: chatroom,
+                   content: payload['message']['content'])
+
+    if message.save
+      ActionCable.server.broadcast "chatroom_#{message.chatroom_id}:messages",
+        ActiveModelSerializers::SerializableResource.new(message).as_json
+    end
+  end
+end
---
+++ db/migrate/20170521225637_create_suggestions.rb
@@ -0,0 +1,13 @@
+class CreateSuggestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :suggestions do |t|
+      t.string :title
+      t.string :description
+      t.string, :api_link
+      t.integer :user_id
+      t.integer :chatroom_id
+
+      t.timestamps
+    end
+  end
+end
