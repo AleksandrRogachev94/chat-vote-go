2a499efe444209b9845930c4eb1bc966358535a2
Aleksandr <aleksandr.rogachev1994@gmail.com>
2017-05-21 15:39:56 -0400

    add authentication checking in actioncable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+        jwt = request.params[:jwt]
+        user = Auth.decode(jwt)
+        User.find(user[:id])
+        rescue JWT::VerificationError, JWT::DecodeError,
+               ActiveRecord::RecordNotFound, JWT::ExpiredSignature
+          reject_unauthorized_connection
+      end
   end
 end
